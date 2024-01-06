b2b92abf2294b4122f1e67b00ee23b80b0ea8b5c
Santiago Mondragon <santiago.mondragon224@gmail.com>
2024-01-05 17:15:33 -0800

    Use new list

--- main.py
+++ main.py
@@ -10,15 +10,10 @@ with open(ALBUMS_JSON_FILE) as f:
 	rand_idx = random.randint(beg, end)
 	selected_album = albums[rand_idx]
 
-	title = selected_album['title']
-	album = selected_album['album']
-	artist = selected_album['artist']
-	year = selected_album['year']
+	album = selected_album['Album']
+	artist = selected_album['Artist']
+	year = selected_album['Date']
 
-	if album and artist:
-		print("Album: ", album)
-		print("Artist: ", artist)
-	else:
-		print("Title: ", title)
-	if year:
-		print("Year: ", year)
+	print("Album: ", album)
+	print("Artist: ", artist)
+	print("Year: ", year)
