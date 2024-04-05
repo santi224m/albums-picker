b4ba65dc60135cc0946a8d624741733d4731962b
Santiago Mondragon <santiago.mondragon224@gmail.com>
2024-04-05 14:31:20 -0700

    refactor main

---
+++ album.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+@dataclass
+class Album:
+  title: str
+  artist: str
+  year: int
--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import json
 import random
 
+from album import Album
+
 ALBUMS_JSON_FILE = "albums.json"
 
-with open(ALBUMS_JSON_FILE) as f:
-	albums = json.load(f)
+def get_rand_album(albums):
+	"""
+	Return a random album
+	"""
 	beg = 0
 	end = len(albums) - 1
 	rand_idx = random.randint(beg, end)
 	selected_album = albums[rand_idx]
-
-	album = selected_album['Album']
+	title = selected_album['Album']
 	artist = selected_album['Artist']
 	year = selected_album['Date']
+	album = Album(title, artist, year)
+	return album
 
-	print("Album: ", album)
-	print("Artist: ", artist)
-	print("Year: ", year)
+def print_album(album):
+	"""Print the choosen album"""	
+	print("Album: ", album.title)
+	print("Artist: ", album.artist)
+	print("Year: ", album.year)
+
+if __name__ == "__main__":
+	with open(ALBUMS_JSON_FILE) as f:
+		albums = json.load(f)
+	
+	rand_album = get_rand_album(albums)
+	print_album(rand_album)
